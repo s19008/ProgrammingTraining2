9c17e70e05b43f37cbd9ff5cc04036993013308f
Yo shinzato <s19008@std.it-college.ac.jp>
2020-05-18 12:37:02 +0900

    kadai

--- chapter01/kadai03.py
+++ chapter01/kadai03.py
@@ -1,2 +1,4 @@
 list = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-print(len(list.split()))
+newtext = list.replace('.', '').replace(',', '')
+result = [len(i) for i in newtext.split()]
+print(result)
